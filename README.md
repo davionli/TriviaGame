97433eaa09f2953754fdc17a00b708e32a1224d6
Davionli <davionli1007@gmail.com>
2019-06-10 04:16:15 -0700

    finish

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # TriviaGame
+
+THIS IS THE LINK https://davionli.github.io/TriviaGame/
+
+For a little multiple choose game.
+
+## How to play
+
+1. Click "START" button to start.
+
+2. Choose the correct answer before time runs out.
+
+## Challenge
+
+How to use the timer to finish this homework.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,46 @@
 //TODO: questions, options, correct answer in array
-var quiz = [{question: "1+1=?", op_1: "1", op_2: "2", op_3: "3", op_4: "4", answer: "op_2"},
-    {question: "1*1=?", op_1: "1", op_2: "2", op_3: "3", op_4: "4", answer: "op_1"},
-    {question: "1-1=?", op_1: "1", op_2: "2", op_3: "3", op_4: "0", answer: "op_4"},
-    {question: "1/1=?", op_1: "1", op_2: "2", op_3: "3", op_4: "4", answer: "op_1"}];
-$(document).ready(function() {
+var quiz = [{question: "6 * 16 = ?", op_1: "86", op_2: "96", op_3: "106", op_4: "66", answer: "op_2"},
+    {question: "Which one is NOT a prime number?", op_1: "1", op_2: "2", op_3: "3", op_4: "5", answer: "op_1"},
+    {question: "587(10) to ???(2)", op_1: "1001001001", op_2: "1001101011", op_3: "1001001111", op_4: "1001001011", answer: "op_4"},
+    {question: "How many cats do I have?", op_1: "0", op_2: "1", op_3: "2", op_4: "3", answer: "op_3"}];
+    var correct = 0;
+    var wrong = 0;
+    var unsolved = 0;
     var quizNum = 0;
     var intervalId;
     var time = 0;
-    var timeForEach = 30;
+    var timeForEach = 3;
     var isTimerRunning = false;
+    var isShowAnswer = false;
+    var temp;
+$(document).ready(function() {
 //TODO: make the timer
 
-
+function resetGame() {
+    quizNum = 0;
+    correct = 0;
+    wrong = 0;
+    unsolved = 0;
+    $("#correct").text("");
+    $("#wrong").text("");
+    $("#unsolved").text("");
+}
 //TODO: click START button
 $("#start").on("click", function() {
-    startTimer();
+    $("#start").css("visibility", "hidden");
+    startTimer(30);
+    nextQuestion();
+});
+$("#start_over").on("click", function() {
+    $("#start_over").css("visibility", "hidden");
+    resetGame();
+    startTimer(30);
     nextQuestion();
 });
 //TODO: timer start countdown
-function startTimer() {
+function startTimer(sec) {
     if (!isTimerRunning){
-        intervalId = setInterval(countdown, 1000);
+        intervalId = setInterval(function() {countdown(sec);}, 1000);
         isTimerRunning = true;
     }
 }
@@ -29,19 +49,41 @@ function resetTimer() {
     time = 0;
     clearInterval(intervalId);
 }
-function countdown() {
+function countdown(sec) {
     time++
-    $("#timer").text(`Time Remaining: ${timeForEach-time} Seconds`);
+    $("#timer").text(`Time Remaining: ${sec-time} Seconds`);
+    if (sec-time===0&&!isShowAnswer) {
+        unsolved++;
+        resetTimer();
+        next();
+    }else if (sec-time===0&&isShowAnswer) {
+        isShowAnswer = false;
+        $(temp).css("background-color", "transparent");
+        resetTimer();
+        next();
+    }
+}
+function showResult() {
+    $("#timer").text("");
+    $("#qst").text("");
+    $("#op-1").text("");
+    $("#op-2").text("");
+    $("#op-3").text("");
+    $("#op-4").text("");
+    $("#correct").text(`Correct: ${correct}`);
+    $("#wrong").text(`Wrong: ${wrong}`);
+    $("#unsolved").text(`Unsolved: ${unsolved}`);
+    $("#start_over").css("visibility", "visible");
 }
 //TODO: create question structure and timer
 function nextQuestion() {
-    $("#timer").text(`Time Remaining: ${timeForEach-time} Seconds`);
+    $("#timer").text(`Time Remaining: ${30-time} Seconds`);
     $("#qst").text(quiz[quizNum].question);
     $("#op-1").text(quiz[quizNum].op_1);
     $("#op-2").text(quiz[quizNum].op_2);
     $("#op-3").text(quiz[quizNum].op_3);
     $("#op-4").text(quiz[quizNum].op_4);
-    startTimer();
+    startTimer(30);
 }
 function next() {
     if (quizNum < quiz.length-1) {
@@ -49,25 +91,42 @@ function next() {
         resetTimer();
         nextQuestion();
     } else {
-        alert("done");
+        showResult();
         resetTimer();
     }
 }
+function showAnswer(isCorrect) {
+    temp = "#" + quiz[quizNum].answer.substr(0, 2) + "-" + quiz[quizNum].answer.substr(3);
+    if (isCorrect) {
+        $(temp).css("background-color", "green");
+    }else {
+        $(temp).css("background-color", "red");
+    }
+    resetTimer();
+    isShowAnswer = true;
+    startTimer(3);
+}
 $(".option").mouseenter(function() {
-    $(this).css("background-color", "red");
+    if (!isShowAnswer)
+        $(this).css("background-color", "lightblue");
 }).mouseleave(function() {
-    $(this).css("background-color", "white");
+    if (!isShowAnswer)
+        $(this).css("background-color", "transparent");
 })
 
 $(".option").on("click", function() {
-    if($(this).attr("data-value") === quiz[quizNum].answer) {
-        alert("right");
-        next();
-    } else {
-        alert("wrong");
-        next();
-    }
+    if (!isShowAnswer) {
+        $(this).css("background-color", "transparent");
+        if($(this).attr("data-value") === quiz[quizNum].answer) {
+            correct++;
+            showAnswer(true);
+        } else {
+            wrong++;
+            showAnswer(false);
+        }
+    }   
 })
+
 
 
 
